timy/texmacs

commit e4f9cc3eb41a5900f3de070e2eefa180c1692990

Cosmetic
git-svn-id: svn://svn.savannah.gnu.org/texmacs/trunk/src@5807 64cb5145-927a-446d-8aed-2fb7b4773692

src/Data/Convert/Texmacs/upgradetm.cpp
@@ -3468,10 +3468,12 @@ upgrade_cyrillic_encoding (tree t, bool cyrillic) {
   else {
     int i = 0;
     if (is_func(t, WITH)) {
-      for (i = 0 ; i < N(t) - 1 ; i++) {
-        if (!cyrillic && become_cyrillic (as_string (t[i]), as_string (t[i+1])))
+      for (i = 0 ; i < N(t) - 1 ; i+=2) {
+        if (!cyrillic
+            && become_cyrillic (as_string (t[i]), as_string (t[i+1])))
           cyrillic = true;
-        else if (cyrillic && become_other (as_string (t[i]), as_string (t[i+1])))
+        else if (cyrillic
+            && become_other (as_string (t[i]), as_string (t[i+1])))
           cyrillic = false;
       }
       t[i] = upgrade_cyrillic_encoding (t[i], cyrillic);

commit 0a49023e9991dd4507f3e76a72f7d177b6c43a04

Fix bug introduced by r5807
git-svn-id: svn://svn.savannah.gnu.org/texmacs/trunk/src@5882 64cb5145-927a-446d-8aed-2fb7b4773692

src/Data/Convert/Texmacs/upgradetm.cpp
@@ -3476,7 +3476,7 @@ upgrade_cyrillic_encoding (tree t, bool cyrillic) {
             && become_other (as_string (t[i]), as_string (t[i+1])))
           cyrillic = false;
       }
-      t[i] = upgrade_cyrillic_encoding (t[i], cyrillic);
+      t[N(t)-1] = upgrade_cyrillic_encoding (t[N(t)-1], cyrillic);
     }
     else {
       for (i = 0 ; i < N(t) ; i++) 